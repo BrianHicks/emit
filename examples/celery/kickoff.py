0d4fcdb0e9c65d2463c6f1b2be61fbe187c6c924
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-07 11:59:56 -0600

    change celery example to use imports

--- examples/celery/kickoff.py
+++ examples/celery/kickoff.py
@@ -1,4 +1,4 @@
-from tasks import emit_words
+from app import router
 import random
 words = 'the rain in spain falls mainly on the plain'.split(' ')
-emit_words(document=' '.join(random.choice(words) for i in range(50)))
+router(document=' '.join(random.choice(words) for i in range(50)))
