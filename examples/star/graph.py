75af1195bd45a35082d68c166526e1073586ebeb
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-08 09:47:16 -0600

    add star routing example

---
+++ examples/star/graph.py
@@ -0,0 +1,21 @@
+from emit import Router
+from emit.message import NoResult
+from redis import Redis
+
+router = Router()
+redis = Redis()
+
+def prefix(name):
+    return '%s.%s' % (__name__, name)
+
+@router.node(('key', 'value'), entry_point=True)
+def parse_querystring(msg):
+    'parse a querystring into keys and values'
+    for part in msg.querystring.strip().lstrip('?').split('&'):
+        key, value = part.split('=')
+        yield key, value
+
+@router.node(tuple(), '*')
+def notify_on_emit(msg):
+    redis.publish('notify_on_emit.%s' % msg._origin, msg.as_json())
+    return NoResult
