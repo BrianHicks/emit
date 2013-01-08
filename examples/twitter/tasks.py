ea54086b9f4a55da37a6ef163af833276c224aa5
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-08 17:51:45 -0600

    add top20 and word count task

--- examples/twitter/tasks.py
+++ examples/twitter/tasks.py
@@ -1,20 +1,24 @@
 from redis import Redis
 from app import router
+from emit.message import NoResult
 
 redis = Redis()
 
 @router.node(['tweet'], entry_point=True)
 def tweet_text(msg):
     'given a twitter API response, get the tweet content'
-    if 'text' in msg.response:
-        return msg.response['text']
+    return msg.response.get('text', NoResult)
 
 @router.node(['word'], 'tasks.tweet_text')
 def clean_words(msg):
     for word in msg.tweet.strip().split(' '):
-        if not word.startswith('@') and not word.startswith('#'):
+        if word and not word.startswith('@') and not word.startswith('#'):
             yield word
 
 @router.node(['word', 'count'], 'tasks.clean_words')
 def tally_word(msg):
     return msg.word, redis.zincrby('tweet_word_counts', msg.word, 1)
+
+@router.node(['message_count'], 'tasks.tweet_text')
+def count_messages(msg):
+    return redis.incr('tweet_count', 1)
---
+++ examples/twitter/top20.py
@@ -0,0 +1,6 @@
+from redis import Redis
+redis = Redis()
+print 'there were %r tweets, here are the top 20 words:' % redis.get('tweet_count')
+print
+for i, (word, score) in enumerate(redis.zrevrange('tweet_word_counts', 0, 19, True)):
+    print '%d:\t%s\t(%d words)' % (i + 1, word, score)
