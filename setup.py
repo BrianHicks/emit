1d42453594aca6b57c0319bb8a83e91013897fd7
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-08 23:55:07 -0600

    add README as long description

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name='emit',
     version=emit.__version__,
     description='Emitter for stream processing',
+    long_description=open('README.md').read(),
     url='http://github.com/brianhicks/emit',
     author='Brian Hicks',
     author_email='brian@brianthicks.com',
