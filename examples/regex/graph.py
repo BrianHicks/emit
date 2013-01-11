fddc4c67d3d4e582c0859dbdb24c6cc5e5df3929
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-10 18:13:15 -0600

    add a count_keyval func to regex example

--- examples/regex/graph.py
+++ examples/regex/graph.py
@@ -5,12 +5,20 @@ from redis import Redis
 router = Router()
 redis = Redis()
 
+def prefix(name):
+    return '%s.%s' % (__name__, name)
+
 @router.node(('key', 'value'), entry_point=True)
 def parse_querystring(msg):
     'parse a querystring into keys and values'
     for part in msg.querystring.strip().lstrip('?').split('&'):
         key, value = part.split('=')
         yield key, value
+
+@router.node(('key', 'value', 'count'), prefix('parse_querystring'))
+def count_keyval(msg):
+    count = redis.zincrby('querystring_count.%s' % msg.key, msg.value, 1)
+    return msg.key, msg.value, count
 
 @router.node(tuple(), '.+')
 def notify_on_emit(msg):
