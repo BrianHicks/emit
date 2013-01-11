e7124f38ca841e2cb42e0ab474cf4a50978df3dd
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-11 16:46:34 -0600

    fix pep8 errors in examples/celery/tasks.py

--- examples/celery/tasks.py
+++ examples/celery/tasks.py
@@ -1,10 +1,12 @@
 from redis import Redis
 from app import app, router
 
+
 @router.node(('word',), entry_point=True)
 def emit_words(msg):
     for word in msg.document.strip().split(' '):
         yield word
+
 
 @router.node(('word', 'count'), subscribe_to='tasks.emit_words', celery_task=app.task(rate_limit='5/s'))
 def tally_word(msg):
