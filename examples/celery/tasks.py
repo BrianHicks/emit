6df4fb4680ed521e71e96a2a8597dd4627d27082
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-10 18:16:28 -0600

    change list to tuples in celery example

--- examples/celery/tasks.py
+++ examples/celery/tasks.py
@@ -1,12 +1,12 @@
 from redis import Redis
 from app import app, router
 
-@router.node(['word'], entry_point=True)
+@router.node(('word',), entry_point=True)
 def emit_words(msg):
     for word in msg.document.strip().split(' '):
         yield word
 
-@router.node(['word', 'count'], subscribe_to='tasks.emit_words', celery_task=app.task(rate_limit='5/s'))
+@router.node(('word', 'count'), subscribe_to='tasks.emit_words', celery_task=app.task(rate_limit='5/s'))
 def tally_word(msg):
     redis = Redis()
     return msg.word, redis.zincrby('celery_emit_example', msg.word, 1)
