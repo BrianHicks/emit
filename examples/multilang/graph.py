ddfa9a5a2278ace08b652d13e35b88eac9375a42
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-10 18:17:12 -0600

    change list to tuples in multilang example

--- examples/multilang/graph.py
+++ examples/multilang/graph.py
@@ -3,10 +3,10 @@ from emit.multilang import ShellNode
 
 router = Router()
 
-@router.node(['n'])
+@router.node(('n',))
 class PythonShellNode(ShellNode):
     command = 'python test.py'
 
-@router.node(['n'])
+@router.node(('n',))
 class RubyShellNode(ShellNode):
     command = 'bundle exec ruby test.rb'
