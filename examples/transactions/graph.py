97ddfc9e63e1fc814d72f3ba2499d35c1d5ae0d6
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-25 14:06:47 -0600

    add transaction example

---
+++ examples/transactions/graph.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+import random
+
+from emit import Router
+from emit.message import NoResult
+from redis import Redis
+
+redis = Redis()
+router = Router()
+
+@router.transaction('ensure_count')
+@router.node(('n',))
+def emit_n(msg):
+    for n in range(msg.count):
+        yield n
+
+def rollback_incr(msgs):
+    for msg in msgs:
+        redis.decr(str(msg.n), 1)
+
+@router.transaction('ensure_count', rollback_incr)
+@router.node(('n', 'count'), 'graph.emit_n')
+def incr(msg):
+    redis.incr(str(msg.n), 1)
+
+@router.transaction('ensure_count')
+@router.node(tuple(), 'graph.emit_n')
+def sometimes_fail(msg):
+    if random.random() > 0.8:
+        raise ValueError('random was greater than 0.8')
+
+    return NoResult
