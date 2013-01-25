d59c6bddb55cc4a952d2cedb7d3e6c203eca5b0b
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-25 15:11:16 -0600

    test limiting transactions to nodes

--- tests/router_transaction_tests.py
+++ tests/router_transaction_tests.py
@@ -1,1 +1,27 @@
 'tests for transactions in the router'
+from unittest import TestCase
+
+from emit import Router
+
+
+class MetaTests(TestCase):
+    'tests for things outside of the actual transaction handling'
+    def setUp(self):
+        self.router = Router()
+
+    def test_only_works_on_nodes(self):
+        'transaction decorator only works on nodes'
+        def func(msg):
+            return msg.x
+
+        transaction = self.router.transaction('test')
+        try:
+            self.assertRaisesRegexp(
+                ValueError, 'transactions may only be applied to nodes',
+                transaction, func
+            )
+        except AttributeError:  # python 2.6
+            self.assertRaises(
+                ValueError,
+                transaction, func
+            )
