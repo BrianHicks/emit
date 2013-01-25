200101c798428d95070b08c2adc8e3a54ff9c04f
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-25 14:36:30 -0600

    fix for pep8 compatibility

--- examples/transactions/graph.py
+++ examples/transactions/graph.py
@@ -8,20 +8,24 @@ from redis import Redis
 redis = Redis()
 router = Router()
 
+
 @router.transaction('ensure_count')
 @router.node(('n',))
 def emit_n(msg):
     for n in range(msg.count):
         yield n
 
+
 def rollback_incr(msgs):
     for msg in msgs:
         redis.decr(str(msg.n), 1)
+
 
 @router.transaction('ensure_count', rollback_incr)
 @router.node(('n', 'count'), 'graph.emit_n')
 def incr(msg):
     redis.incr(str(msg.n), 1)
+
 
 @router.transaction('ensure_count')
 @router.node(tuple(), 'graph.emit_n')
