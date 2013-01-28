1c34893f9abfdb8bc460342173781bf4ad37bbf9
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-28 15:01:04 -0600

    add tests for add_transaction

--- tests/transaction_base_tests.py
+++ tests/transaction_base_tests.py
@@ -23,3 +23,36 @@ def single_notimplemented(method, args):
         assert False, 'NotImplementedError not raised'
     except NotImplementedError:
         pass
+
+
+class AddTransactionTests(TestCase):
+    'tests for BaseTransactionHandler.add_transaction'
+    def setUp(self):
+        self.handler = BaseTransactionHandler()
+
+    def test_new(self):
+        'creates and adds to a set'
+        self.handler.add_transaction('trans_name', 'func_name')
+
+        self.assertEqual(
+            set(['trans_name']),
+            self.handler.transactions['func_name']
+        )
+
+    def test_existing(self):
+        'adds to a set'
+        self.handler.transactions['func_name'] = set(['test'])
+        self.handler.add_transaction('trans_name', 'func_name')
+
+        self.assertEqual(
+            set(['trans_name', 'test']),
+            self.handler.transactions['func_name']
+        )
+
+    def test_returns(self):
+        'returns new value'
+
+        self.assertEqual(
+            set(['trans_name']),
+            self.handler.add_transaction('trans_name', 'func_name')
+        )
