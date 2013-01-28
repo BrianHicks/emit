71128beb0c6f6f74a99498a4f7308ef3d2520bd3
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-28 14:49:28 -0600

    add tests for NotImplemented methods

---
+++ tests/transaction_base_tests.py
@@ -0,0 +1,23 @@
+'tests for Base transaction handler'
+from unittest import TestCase
+
+from emit import Router
+from emit.transactions.base import BaseTransactionHandler
+
+def test_notimplemented():
+    'test methods which have to be implemented by subclasses'
+    methods = [
+        ('start_call', ('a', 'b', 'c')),
+        ('finish_call', ('a', 'b')),
+        ('rollback', ('a', 'b')),
+    ]
+    for method, args in methods:
+        yield single_notimplemented, method, args
+
+def single_notimplemented(method, args):
+    'test a single notimplemented'
+    try:
+        getattr(BaseTransactionHandler(), method)(*args)
+        assert False, 'NotImplementedError not raised'
+    except NotImplementedError:
+        pass
