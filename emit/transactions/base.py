4adad5e6213e3e1af17f60c38ac72979383f6242
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-28 14:28:54 -0600

    fill out stubs for TransactionHandler

--- emit/transactions/base.py
+++ emit/transactions/base.py
@@ -1,3 +1,19 @@
 class TransactionHandler(object):
     def __init__(self):
         self.rollback_functions = {}
+
+    def set_transaction_id(self, message):
+        'set a transaction ID'
+        raise NotImplementedError
+
+    def start_call(self, transaction, name, message):
+        'start a transactional call'
+        raise NotImplementedError
+
+    def finish_call(self, transaction, name):
+        'finish a call'
+        raise NotImplementedError
+
+    def rollback(self, transaction, name):
+        'roll back a transaction'
+        raise NotImplementedError
