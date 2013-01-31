c40a40c8b955dcaeb9959bdff84fc22044b5344b
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-31 16:35:21 -0600

    move tests to own file

--- tests/router_core_tests.py
+++ tests/router_core_tests.py
@@ -8,13 +8,12 @@ from .utils import skipIf
 import mock
 from redis import Redis
 
-from emit.router.core import Router, CeleryRouter
+from emit.router.core import Router
 from emit.messages import Message, NoResult
 
-try:
-    from celery import Celery, Task
-except ValueError:  # Celery doesn't work under Python 3.3 - when it does it'll test again
-    Celery = None
+
+def prefix(name):
+    return '%s.%s' % (__name__, name)
 
 
 def get_named_mock(name):
@@ -22,10 +21,6 @@ def get_named_mock(name):
     m.__name__ = name
     m.name = name
     return m
-
-
-def prefix(name):
-    return '%s.%s' % (__name__, name)
 
 
 class ResolveNodeModulesTests(TestCase):
@@ -534,61 +529,3 @@ class RouterTests(TestCase):
         func(n=1)
 
         self.assertEqual(0, watcher.call_count)
-
-
-@skipIf(Celery is None, 'Celery did not import correctly')
-class CeleryRouterTests(TestCase):
-    'tests for using celery to route nodes'
-    def setUp(self):
-        self.celery = self.get_test_celery()
-        self.router = CeleryRouter(self.celery.task)
-
-    def get_test_celery(self):
-        celery = Celery()
-        celery.conf.update(
-            CELERY_ALWAYS_EAGER=True
-        )
-        return celery
-
-    def test_registers_as_task(self):
-        'registers the function as a task'
-        l = lambda x: x
-        l.__name__ = 'test'
-        self.router.node(['test'], celery_task=self.celery.task)(l)
-
-        self.assertTrue(
-            isinstance(self.router.functions[prefix('test')], Task)
-        )
-
-    def test_adds_when_initialized(self):
-        'if router is passed a celery task when initialized it wraps with it'
-        r = CeleryRouter(celery_task=self.celery.task)
-
-        l = lambda x: x
-        l.__name__ = 'test'
-        r.node(['test'])(l)
-
-        self.assertTrue(
-            isinstance(r.functions[prefix('test')], Task)
-        )
-
-    def test_calls_delay(self):
-        'calls delay to route'
-        func = lambda n: n
-        func.name = 'name'
-        self.router.node(tuple(), entry_point=True)(func)
-
-        node = mock.Mock()  # replace node with mock to test call
-        self.router.functions['name'] = node
-
-        self.router(x=1)
-
-        node.delay.assert_called_with(_origin='__entry_point', x=1)
-
-    def test_get_name_celery(self):
-        'gets the name of a celery-decorated function'
-        l = lambda x: x
-        l.__name__ = 'test'
-        l = self.celery.task(l)
-
-        self.assertEqual(prefix('test'), self.router.get_name(l))
